ec1f6919a1808fb364d405814d66b248811f3b71
shane donnelly <shane@donnelly>
2025-06-02 17:49:58 +0300

    to create the configs

---
+++ setup.py
@@ -0,0 +1,16 @@
+import configparser
+
+
+def create_config():
+    config = configparser.ConfigParser()
+
+    # Add sections and key-value pairs
+    config['General'] = {'max_unique': 50}
+
+    # Write the configuration to a file
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+
+if __name__ == "__main__":
+    create_config()
